69529eca1753fc7705b98664e2c6904fd7c41b25
JhonnyAJ <55931058+JhonnyAJ@users.noreply.github.com>
2021-04-01 12:23:10 -0600

    Add files via upload

---
+++ htdocs/routes.php
@@ -0,0 +1,10 @@
+<?php
+    
+    Route::get('/',function() { return view('principal'); });
+    Route::get('/libro',function() { return view('libro'); });
+    Route::get('/libro2',function() { return view('libro2'); });
+    Route::get('/libro3',function() { return view('libro3'); });
+    Route::get('/libro4',function() { return view('libro4'); });
+
+    Route::dispatch();
+?>
